ae5d814cb3d8da415be6304cbf6f6ec1ace7747b
lucasdsmachado <briandsmachado@gmail.com>
2023-03-06 22:05:54 -0300

    review: compare strings containing backspaces

--- two pointers/backspace_compare.js
+++ two pointers/backspace_compare.js
@@ -1,39 +1,46 @@
-const backspace_compare = function(s, t) {
-  let sLastIndex = s.length - 1, tLastIndex = t.length - 1;
+const backspace_compare = function (s, t) {
+  let index1 = s.length - 1, index2 = t.length - 1;
 
-  while (sLastIndex >= 0 || tLastIndex >= 0) {
-    let hashCount = 0;
+  while (index1 >= 0 || index2 >= 0) {
+    let i1 = get_next_valid_char_index(s, index1),
+      i2 = get_next_valid_char_index(t, index2);
 
-    while (s[sLastIndex] === '#') {
-      hashCount++;
-      while (hashCount > 0 && s[sLastIndex - 1] != '#') {
-        hashCount--;
-        sLastIndex--;
-      }
-      sLastIndex--;
+    if (i1 < 0 && i2 < 0) { // REACHED THE END OF BOTH THE STRINGS
+      return true;
     }
 
-    while (t[tLastIndex] === '#') {
-      hashCount++;
-      while (hashCount > 0 && t[tLastIndex - 1] != '#') {
-        hashCount--;
-        tLastIndex--;
-      }
-      tLastIndex--;
-    }
-
-    if (s[sLastIndex] != t[tLastIndex]) {
+    if (i1 < 0 || i2 < 0) { // REACHED THE END OF ONE OF THE STRINGS
       return false;
     }
 
-    sLastIndex--;
-    tLastIndex--;
+    if (s[i1] !== t[i2]) { // CHECK IF CHARACTERS ARE EQUAL
+      return false;
+    }
+
+    index1 = i1 - 1;
+    index2 = i2 - 1;
   }
 
   return true;
 }
 
-console.log(backspace_compare("y#fo##f", "y#f#o##f")); // false
+const get_next_valid_char_index = function (str, index) {
+  let backspaceCount = 0;
+  while (index >= 0) {
+    if (str[index] === '#') { // FOUND A BACKSPACE CHARACTER
+      backspaceCount++;
+    } else if (backspaceCount > 0) { // A NON-BACKSPACE CHARACTER
+      backspaceCount--;
+    } else {
+      break;
+    }
+    index--; // SKIP A BACKSPACE OR A VALID CHARACTER
+  }
+
+  return index;
+}
+
+console.log(backspace_compare("y#fo##f", "y#f#o##f")); // true
 console.log(backspace_compare("bxj##tw", "bxo#j##tw")); // true
 console.log(backspace_compare("ab##", "c#d#")); // true
 console.log(backspace_compare("xp#", "xyz##")); // true
