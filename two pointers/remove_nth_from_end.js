a4cb27146a39d05e4859a2e1be4efcfc9337c3f3
lucasdsmachado <briandsmachado@gmail.com>
2024-01-15 15:09:31 -0300

    done: remove nth node from end of list

--- two pointers/remove_nth_from_end.js
+++ two pointers/remove_nth_from_end.js
@@ -1,33 +1,44 @@
-const remove_nth_from_end = function (head, n) {
-  if (head === null || (head.next === null && n >= 0)) {
-    return null;
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
   }
 
-  let lastNode = reverse(head),
-    copyLastNode = lastNode,
-    prev = null;
-
-  if (n === 1) {
-    copyLastNode = copyLastNode.next;
-    lastNode = null;
-  } else {
-    for (let i = 0; i < n - 1; i++) {
-      prev = lastNode
-      lastNode = lastNode.next;
+  printList() {
+    let result = "";
+    let temp = this;
+    while (temp !== null) {
+      result += temp.value + " ";
+      temp = temp.next;
     }
-    prev.next = lastNode.next;
+    console.log(result);
+  }
+}
+
+const remove_nth_from_end = function (head, n) {
+  let left = head, right = head;
+  for (let i = 0; i < n; i++) {
+    right = right.next;
   }
 
-  return reverse(copyLastNode);
+  if (!right) {
+    return head.next;
+  }
+
+  while (right.next !== null) {
+    left = left.next;
+    right = right.next;
+  }
+
+  left.next = left.next.next;
+
+  return head;
 };
 
-const reverse = function (head) {
-  let prev = null, next = null;
-  while (head !== null) {
-    next = head.next;
-    head.next = prev;
-    prev = head;
-    head = next;
-  }
-  return prev;
-}
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+remove_nth_from_end(head, 2);
+head.printList();
