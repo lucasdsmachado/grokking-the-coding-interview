1c247fae1b360fb7bcc2e0b64aa07b40738b6e4f
lucasdsmachado <briandsmachado@gmail.com>
2024-01-16 18:35:05 -0300

    done: merge intervals

--- merge intervals/merge.intervals.js
+++ merge intervals/merge.intervals.js
@@ -1,23 +1,27 @@
 function merge(intervals) {
-  if (intervals.length === 0 || intervals.length[0] === 0) {
-    return [];
+  if (intervals.length < 2) {
+    return intervals;
   }
-
+  // sort the intervals on the start time
   intervals.sort((a, b) => a[0] - b[0]);
-  let merged = [intervals[0]];
+
+  let mergedIntervals = [];
+  let start = intervals[0][0],
+    end = intervals[0][1];
 
   for (let i = 1; i < intervals.length; i++) {
-    let prev = merged.pop(), curr = intervals[i];
-    if (prev[1] >= curr[0] && prev[1] <= curr[1]) { // curr ends after
-      merged.push([prev[0], curr[1]]);
-    } else if (prev[1] >= curr[0] && prev[1] >= curr[1]) { // prev contains curr
-      merged.push(prev);
-    } else { // disjoint
-      merged.push(prev, curr);
+    const interval = intervals[i];
+    if (interval[0] <= end) {
+      end = Math.max(interval[1], end); // overlapping intervals, adjust the end
+    } else { // non-overlapping intervals, add the previous interval and reset
+      mergedIntervals.push([start, end]);
+      start = interval[0];
+      end = interval[1];
     }
   }
-
-  return merged;
+  // add the last interval
+  mergedIntervals.push([start, end]);
+  return mergedIntervals;
 }
 
 console.log(merge([[1, 3], [2, 6], [8, 10], [15, 18]])); // [[1,6],[8,10],[15,18]]
