f93162e9b9e559c2a607c97ba58db7b2fb00e541
lucasdsmachado <briandsmachado@gmail.com>
2024-01-26 12:41:27 -0300

    done: maximum cpu load

--- merge intervals/maximum_cpu_load.js
+++ merge intervals/maximum_cpu_load.js
@@ -1,21 +1,27 @@
+import Heap from 'collections/heap.js';
+
 const find_max_cpu_load = function (jobs) {
   jobs.sort((a, b) => a[0] - b[0]);
-  if (jobs.length === 0) {
-    return 0;
-  }
-  let maxCPULoad = jobs[0][2];
-  for (let i = 1; i < jobs.length; i++) {
-    if (jobs[i - 1][1] > jobs[i][0]) {
-      jobs[i][0] = jobs[i - 1][0];
-      jobs[i][1] = Math.max(jobs[i][1], jobs[i - 1][1]);
-      jobs[i][2] = jobs[i - 1][2] + jobs[i][2];
-    }
-    maxCPULoad = Math.max(maxCPULoad, jobs[i][2]);
-  }
 
+  let maxCPULoad = 0,
+    currentCPULoad = 0,
+    minHeap = new Heap([], null, (a, b) => b[1] - a[1]);
+
+  for (let i = 0; i < jobs.length; i++) {
+    while (minHeap.length > 0 && jobs[i][0] >= minHeap.peek()[1])
+      currentCPULoad -= minHeap.pop()[2];
+    minHeap.push(jobs[i]);
+    currentCPULoad += jobs[i][2];
+    maxCPULoad = Math.max(maxCPULoad, currentCPULoad);
+  }
   return maxCPULoad;
 }
 
 console.log(find_max_cpu_load([[1, 4, 3], [2, 5, 4], [7, 9, 6]])); // 7
 console.log(find_max_cpu_load([[6, 7, 10], [2, 4, 11], [8, 12, 15]])); // 15
 console.log(find_max_cpu_load([[1, 4, 2], [2, 4, 1], [3, 6, 5]])); // 8
+console.log(find_max_cpu_load([[2, 4, 5], [0, 6, 7], [5, 10, 6], [0, 3, 10]])); // 22
+console.log(find_max_cpu_load([[2, 4, 5], [0, 6, 7], [5, 10, 6]])) // 13
+console.log(find_max_cpu_load([[1, 3, 50], [2, 4, 10], [3, 5, 40], [3, 6, 70]])); // 120
+console.log(find_max_cpu_load([[1, 3, 20], [2, 5, 20], [3, 10, 100], [4, 6, 70], [6, 9, 60]])); //190
+console.log(find_max_cpu_load([[1, 2, 5], [1, 3, 6], [1, 4, 4]])); // 15
