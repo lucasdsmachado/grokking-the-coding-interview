96cfe8fa65c897b86318b961739f1ec8a39bfed4
lucasdsmachado <briandsmachado@gmail.com>
2024-02-01 13:07:28 -0300

    done: find duplicate

--- cyclic sort/find_duplicate.js
+++ cyclic sort/find_duplicate.js
@@ -1,17 +1,16 @@
 const find_duplicate = function (nums) {
-  let i = 0, duplicate = 0;
+  let i = 0;
   while (i < nums.length) {
-    let correctIdx = nums[i] - 1;
-    if (nums[i] !== nums[correctIdx]) {
-      [nums[i], nums[correctIdx]] = [nums[correctIdx], nums[i]];
-    } else if (i !== correctIdx) {
-      duplicate = nums[i];
-      break;
+    let j = nums[i] - 1;
+    if (nums[i] !== nums[j]) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    } else if (i !== j) {
+      return nums[i];
     } else {
-      i++;
+      i++
     }
   }
-  return duplicate;
+  return -1;
 }
 console.log(find_duplicate([1, 4, 4, 3, 2])); // 4
 console.log(find_duplicate([2, 1, 3, 3, 5, 4])); // 3
